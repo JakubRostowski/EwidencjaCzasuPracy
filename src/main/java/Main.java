b1341e7dad3d39f4d4aeb23c858563cfc3da8529
JakubRostowski <rostowskijakub@gmail.com>
2021-08-19 13:17:51 +0200

    filtering events improved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import Models.Employee;
 import Models.SingleEvent;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
@@ -7,6 +8,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
@@ -17,18 +19,19 @@ public class Main {
         try (CSVReader reader = new CSVReader(new FileReader(pathToCsv))) {
             List<String[]> r = reader.readAll();
             List<SingleEvent> events = new ArrayList<>();
-            int[] indexes = new int[4];
+            int[] indexes = new int[5];
             int actualIndex = 0;
 
             for (String[] singleLine : r) {
                 if (singleLine[0].contains("Parametr 1 zdarzenia RCP - nazwa") || singleLine[0].contains("Data")
-                        || singleLine[0].contains("Godzina") || (singleLine[0].contains("Nazwa u�ytkownika"))) {
+                        || singleLine[0].contains("Godzina") || singleLine[0].contains("Nazwa u�ytkownika")
+                        || singleLine[0].contains("Nazwa zdarzenia")) {
 
                     indexes[actualIndex] = getColumnIndex(singleLine[0]);
                     actualIndex++;
                 }
 
-                if (actualIndex == 4) {
+                if (actualIndex == indexes.length) {
                     break;
                 }
             }
@@ -39,37 +42,49 @@ public class Main {
                 }
                 String[] infoForSingleEvent = singleLine[0].split(";");
 
-                String entryType = infoForSingleEvent[indexes[0]-1];
-                String date = infoForSingleEvent[indexes[1]-1];
-                String time = infoForSingleEvent[indexes[2]-1];
-                String name = infoForSingleEvent[indexes[3]-1];
+                String entryType = infoForSingleEvent[indexes[0] - 1];
+                String accessType = infoForSingleEvent[indexes[1] - 1];
+                String date = infoForSingleEvent[indexes[2] - 1];
+                String time = infoForSingleEvent[indexes[3] - 1];
+                String name = infoForSingleEvent[indexes[4] - 1];
 
-                SingleEvent singleEvent = new SingleEvent(entryType, date, time, name);
-
-                if (!singleEvent.getName().contains("Linia wej�ciowa")) {
+                if (!name.contains("Linia wej�ciowa") && accessType.contains("001")) {
+                    SingleEvent singleEvent = new SingleEvent(entryType, date, time, name);
                     events.add(singleEvent);
                 }
             }
 
             deleteDuplicates(events);
 
-//            for (SingleEvent event : events) {
-//                if (event.getName().equals("Ruszczyk Bo�ena")) {
-//                    System.out.println(event);
-//                }
-//            }
-
-            List<String> names = new ArrayList<>();
             for (SingleEvent event : events) {
-                if (!names.contains(event.getName()) && !event.getName().equals("U�ytkownik nieznany")
-                        && !event.getName().equals("Harmonogram:") && !event.getName().isEmpty()) {
-                    names.add(event.getName());
+                if (event.getName().equals("Ruszczyk Bo�ena")) {
+                    System.out.println(event);
                 }
             }
+
+            List<String> names = extractNames(events);
+            List<Employee> employees = new ArrayList<>();
+
+
+//            for (String name : names) {
+//
+//                employees.add(new Employee(name, daysOfWork));
+//            }
 
         } catch (IOException | CsvException | NullPointerException e) {
             e.printStackTrace();
         }
+    }
+
+    private static List<String> extractNames(List<SingleEvent> events) {
+        List<String> names = new ArrayList<>();
+        for (SingleEvent event : events) {
+            if (!names.contains(event.getName()) && !event.getName().equals("U�ytkownik nieznany")
+                    && !event.getName().equals("Harmonogram:") && !event.getName().isEmpty()) {
+                names.add(event.getName());
+            }
+        }
+        return names;
     }
 
     private static void deleteDuplicates(List<SingleEvent> events) {
--- src/main/java/Models/Employee.java
+++ src/main/java/Models/Employee.java
@@ -1,9 +1,14 @@
 package Models;
 
-
+import java.util.ArrayList;
 
 public class Employee {
 
     private String name;
+    private ArrayList<String[]> logs;
 
+    public Employee(String name, ArrayList<String[]> logs) {
+        this.name = name;
+        this.logs = logs;
+    }
 }
